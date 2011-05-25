31c3fa2cc46d1f08a1e86421bf77bd3fdd0d6fc7
Juxi <juxi@juxis-iMac.(none)>
2011-05-25 17:31:55 +0200

    adding the saliency module, just added the files, does not work yet!!

---
+++ idsiaVisionDemoModule/SaliencyModule/lib/CMakeFiles/libEnvision.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/libEnvision.dir/env_alloc.c.o"
+  "CMakeFiles/libEnvision.dir/env_channel.c.o"
+  "CMakeFiles/libEnvision.dir/env_c_math_ops.c.o"
+  "CMakeFiles/libEnvision.dir/env_image.c.o"
+  "CMakeFiles/libEnvision.dir/env_image_ops.c.o"
+  "CMakeFiles/libEnvision.dir/envision.c.o"
+  "CMakeFiles/libEnvision.dir/envision_lib.c.o"
+  "CMakeFiles/libEnvision.dir/env_job_server.c.o"
+  "CMakeFiles/libEnvision.dir/env_log.c.o"
+  "CMakeFiles/libEnvision.dir/env_math.c.o"
+  "CMakeFiles/libEnvision.dir/env_motion_channel.c.o"
+  "CMakeFiles/libEnvision.dir/env_mt_channel.c.o"
+  "CMakeFiles/libEnvision.dir/env_mt_visual_cortex.c.o"
+  "CMakeFiles/libEnvision.dir/env_params.c.o"
+  "CMakeFiles/libEnvision.dir/env_pthread_interface.c.o"
+  "CMakeFiles/libEnvision.dir/env_pyr.c.o"
+  "CMakeFiles/libEnvision.dir/env_stdio_interface.c.o"
+  "CMakeFiles/libEnvision.dir/env_visual_cortex.c.o"
+  "liblibEnvision.pdb"
+  "liblibEnvision.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/libEnvision.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ idsiaVisionDemoModule/SaliencyModule/lib/CMakeFiles/libEnvision.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for libEnvision.
+# This may be replaced when dependencies are built.
