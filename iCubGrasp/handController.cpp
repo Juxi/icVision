4d99e831face2e922e49e3c89eb985ac0b7e9e3a
Alexander Foerster <alexander@idsia.ch>
2011-07-04 19:37:06 +0200

    hand controller with touch sensor thread

--- iCubGrasp/handController.cpp
+++ iCubGrasp/handController.cpp
@@ -8,9 +8,10 @@
 //}
 HandController::HandController( qreal _errorTolerance, qreal _attenuationFactor ) : 
 																	dd(NULL), pos(NULL), vel(NULL), enc(NULL), pid(NULL), amp(NULL), lim(NULL), tgt(NULL),
-																	palmTouch(false), thumbTouch(false), indexTouch(false), middleTouch(false), littleTouch(false)
+																	palmTouch(false), thumbTouch(false), indexTouch(false), middleTouch(false), ringTouch(false), littleTouch(false)
 {
-	
+	ts = new TouchSenseThread("/touchsensor");
+
 	errorTolerance = _errorTolerance;		// 0-1, where smaller numbers favor precision
 	attenuationFactor = _attenuationFactor;	// 0-inf larger numbers cause the controller to approach target points more slowly
 	
@@ -119,6 +120,12 @@ void HandController::start( const char* _robotName, const char* _partName, int r
 		}
 	}
 	
+	if (!ts->start())
+	{
+		QString err = "Cannot start touch sensor thread";
+		throw err;
+	};
+
 	printf("HandController started successfully.\n");
 }
 
@@ -136,6 +143,8 @@ void HandController::checkValidity()
 
 void HandController::stop()
 {
+	ts->stop();
+
 	checkValidity();
 	
 	/* warning: this for() loop kills the motors: */
@@ -181,6 +190,14 @@ void HandController::grasp( int speed )
 	checkValidity();
 	while ( true )
 	{
+		// update booleans for touch sensors
+		palmTouch = ts->getPalmTouch();
+		thumbTouch = ts->getThumbTouch(); 
+		indexTouch = ts->getIndexTouch();
+		middleTouch = ts->getMiddleTouch();
+		ringTouch = ts->getRingTouch();
+		littleTouch = ts->getLittleTouch();
+		
 		mean = doVelocityControl( speed );
 		msleep(20);
 		
@@ -255,11 +272,11 @@ qreal HandController::doVelocityControl( int speed, bool stopOnTouch )
 	// stop digits that have touched something
 	if ( stopOnTouch )
 	{
-		if ( thumbTouch || indexTouch || middleTouch || littleTouch ) { ERR[7] = 0; }
+		if ( thumbTouch || indexTouch || middleTouch || ringTouch || littleTouch ) { ERR[7] = 0; }
 		if ( thumbTouch ) { ERR[8] = 0; ERR[9] = 0; ERR[10] = 0; }
 		if ( indexTouch ) { ERR[11] = 0; ERR[12] = 0; }
 		if ( middleTouch ) { ERR[13] = 0; ERR[14] = 0; }
-		if ( littleTouch ) { ERR[15] = 0; }
+		if ( littleTouch || ringTouch) { ERR[15] = 0; }
 	}
 	
 	// do feedback velocity control
