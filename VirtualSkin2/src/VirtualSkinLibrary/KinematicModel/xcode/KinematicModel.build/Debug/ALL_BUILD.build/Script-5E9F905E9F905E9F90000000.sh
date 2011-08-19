9d07ca0c82026db70ae5b05d6996d9b83f1b379a
Kail <kail@kailsLinux.(none)>
2011-08-19 13:20:59 +0200

    removed xcode projects

--- VirtualSkin2/src/VirtualSkinLibrary/KinematicModel/xcode/KinematicModel.build/Debug/ALL_BUILD.build/Script-5E9F905E9F905E9F90000000.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/kail/imClever/dev/VirtualSkin2/src/VirtualSkinLibrary/KinematicModel/xcode -f /Users/kail/imClever/dev/VirtualSkin2/src/VirtualSkinLibrary/KinematicModel/xcode/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
