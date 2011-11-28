6759841501409e2931b17a4db5329b255a0e0ca0
Kail Frank <kail@idsia.ch>
2011-11-28 16:06:08 +0100

    added iCubBabble

---
+++ iCubBabble/xcode/iCubBabbler.build/Debug/ALL_BUILD.build/Script-56BD2056BD2056BD20000000.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/kail/imClever/dev/iCubBabble/xcode -f /Users/kail/imClever/dev/iCubBabble/xcode/CMakeScripts/ReRunCMake.make
---
+++ iCubBabble/xcode/iCubBabbler.build/Debug/iCubBabble.build/Script-5EA0905EA0905EA090000000.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/kail/imClever/dev/iCubBabble/xcode -f /Users/kail/imClever/dev/iCubBabble/xcode/CMakeScripts/iCubBabble_cmakeRulesBuildPhase.make$CONFIGURATION all
