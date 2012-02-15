d951c0617805d536fd9c8a245bf1308814f1055c
Marijn Stollenga <marijn@idsia.ch>
2012-02-15 10:55:29 +0100

    poses reader

---
+++ VirtualSkin/src/VirtualSkinLibrary/KinematicModel/GraspFind/poses_reader.h
@@ -0,0 +1,52 @@
+#ifndef __POSES_READER__
+#define __POSES_READER__
+
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <vector>
+#include <string>
+#include <cctype>
+#include <algorithm>
+
+#include <iostream>
+
+typedef std::vector<std::vector<float> > poses_vector_t;
+typedef std::map<std::string, poses_vector_t> poses_map_t;
+
+poses_map_t read_poses(std::string filename) {
+	std::ifstream in_file(filename.c_str());
+
+	poses_map_t the_map;
+	poses_vector_t the_poses;
+
+	std::string current_name;
+	while (true) {
+		std::string line;
+		getline(in_file, line);
+		if (!in_file)
+			break;
+
+		if (std::isalpha(line[0])) {
+			if (current_name.size())
+				the_map[current_name] = the_poses;
+			std::remove(line.begin(), line.end(), ' ');
+			current_name = line;
+		} else {
+			std::istringstream line_reader(line);
+			std::vector<float> pose;
+			while (true) {
+				float number;
+				line_reader >> number;
+				if (!line_reader)
+					break;
+				pose.push_back(number);
+			}
+			the_poses.push_back(pose);
+		}
+	}
+	the_map[current_name] = the_poses;
+	return the_map;
+}
+
+#endif
