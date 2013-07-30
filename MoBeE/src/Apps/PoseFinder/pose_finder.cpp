c3a188e026adaef03958da4d269d4f11b9ba48e7
kail frank <kailfrank@gmail.com>
2013-07-30 13:41:28 +0200

    posefinder

--- MoBeE/src/Apps/PoseFinder/pose_finder.cpp
+++ MoBeE/src/Apps/PoseFinder/pose_finder.cpp
@@ -59,54 +59,52 @@ void PoseFinder::run()
     
     keepRunning = true;
     
-    Point_d x=q, y=q, z=q;
-    QVector3D dx(1,0,0), dy(0,1,0), dz(0,0,1);
-    
-   // std::pair<QVector3D,Point_d>
-    
+    Point_d x=q, y=q, z=q, nx, ny, nz;
+    double xl=0.0,yl=0.0,zl=0.0;
+   
+    std::vector<Crawler> crawlers;
+    crawlers.push_back(Crawler(this,q,Vector_3(1,0,0),Qt::blue));
+    crawlers.push_back(Crawler(this,q,Vector_3(0,1,0),Qt::green));
+    crawlers.push_back(Crawler(this,q,Vector_3(0,0,1),Qt::red));
+    crawlers.push_back(Crawler(this,q,Vector_3(-1,0,0),Qt::blue));
+    crawlers.push_back(Crawler(this,q,Vector_3(0,-1,0),Qt::green));
+    crawlers.push_back(Crawler(this,q,Vector_3(0,0,-1),Qt::red));
+                                          
 	while ( keepRunning )
 	{
-        x = next(x, Vector_3(1,0,0), Qt::blue);
-        y = next(y, Vector_3(0,1,0), Qt::green);
-        z = next(z, Vector_3(0,0,1), Qt::red);
-	}
-}
-
-
-Point_d PoseFinder::next(Point_d q, Vector_3 delta, QColor c)
-{
-    CGAL::Random_points_on_sphere_d<Point_d> localSphere(bodypart->size(), 0.1);
-    double max = -1.0;
-    double len = 0.0;
-    setNormPose(q);
-    QVector3D marker_pose = marker->node()->getPos();
-    
-    KinematicModel::CompositeObject* obj = new KinematicModel::CompositeObject( model.OBSTACLE(), model.GHOST() );
-    KinematicModel::PrimitiveObject* primitive = new KinematicModel::Sphere( 0.01 );
-    primitive->setCollidingColor(c);
-    primitive->setFreeColor(c);
-    primitive->translate(marker_pose);
-    obj->appendPrimitive(primitive);
-    model.appendObject(obj);
-    
-    Vector_d winner = *localSphere - CGAL::ORIGIN;
-    printf("----------------------\n");
-    for (int n=0; n<1000; n++) {
-        //printf("..%d",n);
-        Vector_d candidate = *localSphere++ - CGAL::ORIGIN;
-        //std::cout << "  candidate: " << candidate << std::endl;
-        setNormPose(q + candidate);
-        QVector3D new_marker_pose = marker->node()->getPos();
-        QVector3D dx = new_marker_pose - marker_pose;
-        double dot_prod = QVector3D::dotProduct(QVector3D(delta.x(),delta.y(),delta.z()), dx/dx.length());
-        //std::cout << "  dot_prod: " << dot_prod << std::endl;
-        if ( dot_prod > max) {
-            max = dot_prod;
-            len = dx.length();
-            winner = candidate;
+        std::vector<Crawler> newCrawlers;
+        for (std::vector<Crawler>::iterator i = crawlers.begin(); i!= crawlers.end(); ++i )
+        {
+            q = i->next();
+            if (i->getLen()>0.1)
+            {
+                if (i->getDelta() == Vector_3(1,0,0) || i->getDelta() == Vector_3(-1,0,0) ) {
+                    newCrawlers.push_back(Crawler(this,q,Vector_3(0,1,0),Qt::green));
+                    newCrawlers.push_back(Crawler(this,q,Vector_3(0,0,1),Qt::red));
+                    newCrawlers.push_back(Crawler(this,q,Vector_3(0,-1,0),Qt::green));
+                    newCrawlers.push_back(Crawler(this,q,Vector_3(0,0,-1),Qt::red));
+                }
+                else if (i->getDelta() == Vector_3(0,1,0) || i->getDelta() == Vector_3(0,-1,0)) {
+                    newCrawlers.push_back(Crawler(this,q,Vector_3(1,0,0),Qt::blue));
+                    newCrawlers.push_back(Crawler(this,q,Vector_3(0,0,1),Qt::red));
+                    newCrawlers.push_back(Crawler(this,q,Vector_3(-1,0,0),Qt::blue));
+                    newCrawlers.push_back(Crawler(this,q,Vector_3(0,0,-1),Qt::red));
+                }
+                else if (i->getDelta() == Vector_3(0,0,1) || i->getDelta() == Vector_3(0,0,-1)) {
+                    newCrawlers.push_back(Crawler(this,q,Vector_3(1,0,0),Qt::blue));
+                    newCrawlers.push_back(Crawler(this,q,Vector_3(0,1,0),Qt::green));
+                    newCrawlers.push_back(Crawler(this,q,Vector_3(-1,0,0),Qt::blue));
+                    newCrawlers.push_back(Crawler(this,q,Vector_3(0,-1,0),Qt::green));
+                }
+            }
+            
         }
-    }
-    return q + winner;
+        
+        printf("\n");
+        
+        for (std::vector<Crawler>::iterator i = newCrawlers.begin(); i!= newCrawlers.end(); ++i )
+            crawlers.push_back(*i);
+	}
 }
 
 Vector_d PoseFinder::dDelta( bool advance ){
@@ -121,5 +119,55 @@ void PoseFinder::setNormPose( Point_d q ) {
         cmd.push_back(*i);
     bodypart->setNormPos(cmd);
     model.computePose();
+}
+
+Crawler::Crawler(PoseFinder* _finder, Point_d _q, Vector_3 _delta, QColor _c): finder(_finder),q(_q),delta(_delta),c(_c),len(0.0) //,localSphere(_q.dimension(), 0.1)
+{
+    //CGAL::Random_points_on_sphere_d<Point_d> localSphere(finder->bodypart->size(), 0.1);
+}
+Point_d Crawler::next()
+{
+    CGAL::Random_points_on_sphere_d<Point_d> localSphere(finder->bodypart->size(), 0.1);
+    double max = -1.0;
+    //double len = 0.0;
+    finder->setNormPose(q);
+    QVector3D marker_pose = finder->marker->node()->getPos();
+    
+    KinematicModel::CompositeObject* obj = new KinematicModel::CompositeObject( finder->model.OBSTACLE(), finder->model.GHOST() );
+    KinematicModel::PrimitiveObject* primitive = new KinematicModel::Sphere( 0.01 );
+    primitive->setCollidingColor(c);
+    primitive->setFreeColor(c);
+    primitive->translate(marker_pose);
+    obj->appendPrimitive(primitive);
+    finder->model.appendObject(obj);
+    
+    //printf("----------------------\n");
+    Vector_d winner = *localSphere - CGAL::ORIGIN;
+    QVector3D winner_dx(0,0,0);
+    for (int n=0; n<1000; n++)
+    {
+                //printf("..%d",n);
+        Vector_d candidate = *localSphere++ - CGAL::ORIGIN;
+                //std::cout << "  candidate: " << candidate << std::endl;
+        finder->setNormPose(q + candidate);
+        QVector3D new_marker_pose = finder->marker->node()->getPos();
+        QVector3D dx = new_marker_pose - marker_pose;
+            //printf(" dx(%f %f %f) = new_pose(%f %f %f) - old_pose(%f %f %f)\n", dx.x(),dx.y(),dx.z(),new_marker_pose.x(),new_marker_pose.y(),new_marker_pose.z(),marker_pose.x(),marker_pose.y(),marker_pose.z());
+            //printf("dx.len() = %f\n",dx.length());
+        double dot_prod = QVector3D::dotProduct(QVector3D(delta.x(),delta.y(),delta.z()), dx/dx.length());
+                //std::cout << "  dot_prod: " << dot_prod << std::endl;
+        if ( dot_prod > max) {
+            max = dot_prod;
+            winner = candidate;
+            winner_dx = dx;
+        }
+    }
+    //printf("delta: %f %f %f len: %f\n", delta.x(), delta.y(), delta.z(), len);
+    q = q + winner;
+    //printf("                len(%f) += dx_len(%f)\n",len,winner_dx.length());
+    len += winner_dx.length();
+    //printf("                len = %f\n",len);
+    
+    return q;
 }
 
--- MoBeE/src/Apps/PoseFinder/pose_finder.h
+++ MoBeE/src/Apps/PoseFinder/pose_finder.h
@@ -15,6 +15,36 @@
 #include "util.h"
 
 //class KinematicModel::Model;
+class PoseFinder;
+
+class Crawler
+{
+    
+public:
+    Crawler(PoseFinder* _finder,
+            Point_d _q,
+            Vector_3 _delta,
+            QColor _c = Qt::white);
+    ~Crawler()
+    {
+    }
+    
+    Point_d next();
+    double getLen() {return len;}
+    Vector_3 getDelta() {return delta;}
+    Point_d get_q() { return q; }
+    
+private:
+    PoseFinder* finder;
+    Point_d q;
+    Vector_3 delta;
+    QColor c;
+    double len;
+    //KinematicModel::Model* model;
+    //KinematicModel::BodyPart* bodypart;
+    //CGAL::Random_points_on_sphere_d<Point_d> localSphere;
+};
+
 
 class PoseFinder : public QThread
 {
@@ -27,6 +57,8 @@ public:
 	void stop();
 	
 private:
+    
+    friend class Crawler;
 
     KinematicModel::Model                       model;	// the world model
     KinematicModel::Robot*                      robot;
@@ -37,7 +69,7 @@ private:
     bool keepRunning;
     
 	void run();
-    Point_d next(Point_d q, Vector_3 delta, QColor c = Qt::green );
+    //Point_d next(Point_d q, Vector_3 delta, QColor c = Qt::green );
     
     Vector_d dDelta( bool advance = true);
 };
